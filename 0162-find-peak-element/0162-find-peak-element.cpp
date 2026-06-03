a63e46f33e2e5be053cfdcbdf6d7619a79c7af86
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-03 12:32:39 +0530

    Time: 0 ms (100%), Space: 12.5 MB (70.89%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,4 +1,28 @@
-// 1.Brute
+// // 1.Brute
+// class Solution {
+// public:
+//     int findPeakElement(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1 || nums[0]>nums[1])
+//         {
+//             return 0;
+//         }
+//         if(nums[n-1]>nums[n-2])
+//         {
+//             return n-1;
+//         }
+//         for(int i=1;i<n-1;i++)
+//         {
+//             if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
+//             {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+// 2.Optimal
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
@@ -11,21 +35,21 @@ public:
         {
             return n-1;
         }
-        for(int i=1;i<n-1;i++)
+        int low=1;
+        int high=n-2;
+        while(low<high)
         {
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
+            int mid = low + (high-low)/2;
+            if(nums[mid]<nums[mid+1])
             {
-                return i;
+                low=mid+1;
             }
+            else
+            {
+                high=mid;
+            }
+            
         }
-        return -1;
+    return low;
     }
 };
-
-// 2.Optimal
-// class Solution {
-// public:
-//     int findPeakElement(vector<int>& nums) {
-        
-//     }
-// };
