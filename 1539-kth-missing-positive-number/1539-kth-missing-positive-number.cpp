f919dac06cfeb5e75113556ddfcdc1adeb522d9c
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-05 10:53:25 +0530

    Time: 0 ms (100%), Space: 13.3 MB (60.24%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,37 +1,54 @@
-// 1.Brute
+// // 1.Brute
+// class Solution {
+// public:
+//     int findKthPositive(vector<int>& arr, int k) {
+//         int maxi = *max_element(arr.begin(),arr.end());
+//         int cnt=0;
+//         int n=arr.size(); 
+//         for(int i=1;i<=maxi+k;)
+//         {
+//         for(int j=0;j<n;)
+//         {
+//             if(arr[j]!=i)
+//             {
+//                 cnt++;
+//             if(cnt==k)
+//             {
+//                 return i;
+//             }
+//             i++;
+//             }
+//             else
+//             {
+//                 j++;
+//                 i++;
+//             }
+          
+//         }
+     
+//         }
+//         return -1;
+//     }
+// };
+// // 2.Brute (Better)
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int maxi = *max_element(arr.begin(),arr.end());
-        int cnt=0;
         int n=arr.size(); 
-        for(int i=1;i<=maxi+k;)
+        for(int i=0;i<n;i++)
         {
-        for(int j=0;j<n;)
-        {
-            if(arr[j]!=i)
-            {
-                cnt++;
-            if(cnt==k)
-            {
-                return i;
-            }
-            i++;
-            }
-            else
-            {
-                j++;
-                i++;
-            }
-          
+          if(arr[i]<=k)
+          {
+            k++;
+          }
+          else 
+            break;
         }
-     
-        }
-        return -1;
+        return k;
     }
 };
 
-// 2.Optimal
+// 3.Optimal
 // class Solution {
 // public:
 //     int findKthPositive(vector<int>& arr, int k) {
