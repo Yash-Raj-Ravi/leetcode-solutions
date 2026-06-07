648f5748e5ad625e80644bfa71c5bb24842ffd19
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-07 16:41:04 +0530

    Time: 4 ms (19.12%), Space: 95.2 MB (60.59%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,78 @@
-// 1.Brute
-class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
-        long double ans=-1;
-        vector<int> merged;
-        
-        for(auto x:nums1)
-        {
-            merged.push_back(x);
-        }
-        for(auto x:nums2)
-        {
-            merged.push_back(x);
-        }
-        sort(merged.begin(),merged.end());
-
-        int sz = merged.size();
-        if(sz%2!=0)
-        {
-            ans = merged[sz/2];
-        }
-        else
-        {
-            ans = (merged[(sz-1)/2] + merged[sz/2])/2.0;
-        }
-        return ans;
-    }
-};
-
+// // 1.Brute
 // class Solution {
 // public:
 //     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//         int m = nums1.size();
+//         int n = nums2.size();
+//         long double ans=-1;
+//         vector<int> merged;
         
+//         for(auto x:nums1)
+//         {
+//             merged.push_back(x);
+//         }
+//         for(auto x:nums2)
+//         {
+//             merged.push_back(x);
+//         }
+//         sort(merged.begin(),merged.end());
+
+//         int sz = merged.size();
+//         if(sz%2!=0)
+//         {
+//             ans = merged[sz/2];
+//         }
+//         else
+//         {
+//             ans = (merged[(sz-1)/2] + merged[sz/2])/2.0;
+//         }
+//         return ans;
 //     }
 // };
+
+// 2.Optimal
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+         int m = nums1.size();
+         int n = nums2.size();
+         if(m>n)
+         {
+            return findMedianSortedArrays(nums2,nums1);
+         }
+         int low=0;
+         int high=m;
+
+         while(low<=high)
+         {
+            int cut1=low +(high-low)/2;
+            int cut2 = (m+n+1)/2 - cut1;
+
+            int l1= (cut1==0)?INT_MIN:nums1[cut1-1];
+            int r1= (cut1==m)?INT_MAX:nums1[cut1];
+            int l2= (cut2==0)?INT_MIN:nums2[cut2-1];
+            int r2= (cut2==n)?INT_MAX:nums2[cut2];
+
+            if(l1<=r2 && l2<=r1)
+            {
+                if((m+n)%2) //odd
+                {
+                    return max(l1,l2);
+                }
+                else
+                {
+                    return (max(l1,l2) + min(r1,r2))/2.0;
+                }
+            }
+            else if(l1>r2)
+            {
+                high = cut1-1;
+            }
+            else
+            {
+                low = cut1+1;
+            }
+         }
+         return 0;
+    }
+};
