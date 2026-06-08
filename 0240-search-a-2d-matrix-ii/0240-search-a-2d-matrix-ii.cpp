579673d9b03a935c761c850e6eeb840fabc2d6e0
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-08 12:17:51 +0530

    Time: 51 ms (69.63%), Space: 18.6 MB (92.58%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -19,45 +19,65 @@
 //     }
 // };
 
-// 2.Better
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows=matrix.size();
-        int cols=matrix[0].size();
-        for(int i=0;i<rows;i++)
-        {
-            if(matrix[i][0]<= target && target<= matrix[i][cols-1])
-            {
-                int low = 0;
-                int high = cols-1;
-                while(low<=high)
-                {
-                    int mid = low + (high-low)/2;
-                    if(matrix[i][mid]==target)
-                    {
-                        return true;
-                    }
-                    else if(matrix[i][mid]<target)
-                    {
-                        low=mid+1;
-                    }
-                    else
-                    {
-                        high=mid-1;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-};
-
-
-
+// // 2.Better
 // class Solution {
 // public:
 //     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
+//         int rows=matrix.size();
+//         int cols=matrix[0].size();
+//         for(int i=0;i<rows;i++)
+//         {
+//             if(matrix[i][0]<= target && target<= matrix[i][cols-1])
+//             {
+//                 int low = 0;
+//                 int high = cols-1;
+//                 while(low<=high)
+//                 {
+//                     int mid = low + (high-low)/2;
+//                     if(matrix[i][mid]==target)
+//                     {
+//                         return true;
+//                     }
+//                     else if(matrix[i][mid]<target)
+//                     {
+//                         low=mid+1;
+//                     }
+//                     else
+//                     {
+//                         high=mid-1;
+//                     }
+//                 }
+//             }
+//         }
+//         return false;
 //     }
 // };
+
+// 3.Optimal
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+         int rows=matrix.size();
+         int cols=matrix[0].size();
+
+         int row=0;
+         int col=cols-1;
+
+         while(row < rows && col>=0)
+         {
+            if(matrix[row][col]==target)
+            {
+                return true;
+            }
+            else if(matrix[row][col]<target)
+            {
+                row++;
+            }
+            else
+            {
+                col--;
+            }
+         }
+         return false;
+    }
+};
