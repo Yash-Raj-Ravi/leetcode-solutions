2d705b33019bbb5e03c2e26394afc371dbb03f01
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-15 09:39:20 +0530

    Time: 0 ms (100%), Space: 10.4 MB (66.22%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,32 +1,49 @@
-class Solution {
-public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-       set<vector<int>> powerset;
-        int n = nums.size();
-        int total = 1 << n;
-        for(int mask=0;mask<total;mask++)
-        {
-            vector<int> ans;
-            for(int i=0;i<n;i++)
-            {
-            if(mask & (1<<i))
-                {
-                    ans.push_back(nums[i]);
-                }
-            }
-            powerset.insert(ans);
-        }
-        vector<vector<int>> ans(powerset.begin(), powerset.end());
-        return ans;
-    }
-};
+// // 1.Brute(Bit-masking)
 // class Solution {
 // public:
 //     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-//        vector<vector<int>>powerset;
-//         vector<int> ans;
-        
-
+//        set<vector<int>> powerset;
+//         int n = nums.size();
+//         int total = 1 << n;
+//         for(int mask=0;mask<total;mask++)
+//         {
+//             vector<int> ans;
+//             for(int i=0;i<n;i++)
+//             {
+//             if(mask & (1<<i))
+//                 {
+//                     ans.push_back(nums[i]);
+//                 }
+//             }
+//             powerset.insert(ans);
+//         }
+//         vector<vector<int>> ans(powerset.begin(), powerset.end());
+//         return ans;
 //     }
 // };
+
+// 2.Recursive
+class Solution {
+public:
+void helper(int index,vector<int>& nums,vector<int>& ans,vector<vector<int>> &powerset)
+{
+    int n= nums.size();
+    powerset.push_back(ans);
+    for(int i=index;i<n;i++)
+    {
+        if(i>index && nums[i-1]==nums[i]) continue;
+
+        ans.push_back(nums[i]);
+        helper(i+1,nums,ans,powerset);
+        ans.pop_back();
+    }
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+       vector<vector<int>>powerset;
+       vector<int> ans;
+       sort(nums.begin(),nums.end());
+       helper(0,nums,ans,powerset);
+       return powerset;
+
+    }
+};
