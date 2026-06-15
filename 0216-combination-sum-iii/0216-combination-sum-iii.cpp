0270e3c1c31da178af58554e93e5ac9243f65ecc
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-15 10:35:59 +0530

    Time: 0 ms (100%), Space: 8.8 MB (60.03%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+
+void helper(int index, vector<vector<int>> &ans,vector<int> &comb,int k,int n)
+{
+    if(k==0 && n==0)
+    {
+        ans.push_back(comb);return;
+    }
+
+    if(k<0 || n<0) return;
+        for(int i=index;i<10;i++)
+        {
+        if(i<=n){
+         comb.push_back(i);
+         helper(i+1,ans,comb,k-1,n-i);
+         comb.pop_back();
+        }
+        else
+         break;
+        
+        }
+        
+}
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> comb;
+        helper(1,ans,comb,k,n);
+        return ans;
+    }
+};
