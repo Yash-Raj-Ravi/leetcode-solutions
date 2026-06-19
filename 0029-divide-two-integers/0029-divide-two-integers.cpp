e86bed181a98858a24cb8a98b8e4f37ab49e5f9b
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-19 07:50:00 +0530

    Time: 3 ms (9.73%), Space: 8.6 MB (71.51%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,3 +1,32 @@
+// 1.Brute
+// class Solution {
+// public:
+//     int divide(int dividend, int divisor) {
+//         if (dividend == divisor) return 1;
+//         if (divisor == 1) return dividend;
+//         if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+
+//         bool positive = (dividend < 0) == (divisor < 0);
+
+//         long long p = llabs((long long)dividend);
+//         long long q = llabs((long long)divisor);
+
+//         long long count = 0;
+
+//         while (p >= q) {
+//             p -= q;
+//             count++;
+//         }
+
+//         if (count > INT_MAX) {
+//             return positive ? INT_MAX : INT_MIN;
+//         }
+
+//         return positive ? (int)count : -(int)count;
+//     }
+// };
+
+// 2.Optimal
 class Solution {
 public:
     int divide(int dividend, int divisor) {
@@ -11,10 +40,19 @@ public:
         long long q = llabs((long long)divisor);
 
         long long count = 0;
+        long long multiple = 1;
+        long long temp=q;
 
         while (p >= q) {
-            p -= q;
-            count++;
+        long long multiple = 1;
+        long long temp=q;
+          while((temp << 1)<=p)
+          {
+            temp<<=1;
+            multiple<<=1;
+          }
+          count+=multiple;
+          p-=temp;
         }
 
         if (count > INT_MAX) {
