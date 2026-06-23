d3014e4857e58a55d8af39144f7fb614fde76b23
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-23 12:56:32 +0530

    Update update_readme.py

--- .github/workflows/scripts/update_readme.py
+++ .github/workflows/scripts/update_readme.py
@@ -3,19 +3,17 @@ from datetime import datetime
 
 count = 0
 
-for item in os.listdir("."):
-    if os.path.isdir(item):
-        if item[0].isdigit():
-            count += 1
+for item in os.listdir():
+    if os.path.isdir(item) and item[:4].isdigit():
+        count += 1
 
-readme = f"""# LeetCode Solutions
+content = f"""# LeetCode Solutions
 
 ## Statistics
 
 - Total Problems Solved: {count}
 - Last Updated: {datetime.now().strftime("%d %B %Y")}
-
 """
 
 with open("README.md", "w") as f:
-    f.write(readme)
+    f.write(content)
