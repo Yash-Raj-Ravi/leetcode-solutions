22dc4c9ab6cb86eb95b29eca06403d3807739b4b
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-24 10:31:12 +0530

    Time: 0 ms (100%), Space: 10.3 MB (98.66%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,59 +2,53 @@
 // class Solution {
 // public:
 //     int lengthOfLongestSubstring(string s) {
-//            int len=1;
-//            int maxlen=0;
-//         for(int i=0;i<s.size()-1;)
+//         int n=s.size();
+//         int maxlen=0;
+//         for(int i=0;i<n;i++)
 //         {
-//           for(int j=1;j<s.size();)
+//             vector<int> hash(256,0);
+//             for(int j=i;j<n;j++)
 //             {
-//                 if(s[i]!=s[j] && s.find[j]==string::npos)
-//                 {
-//                     len++;
-//                     j++;
-//                     maxlen=max(maxlen,len);
+//                 if(hash[s[j]]==1) {
+//                     break;// since character already present thus duplicate
 //                 }
-//                 else
-//                 {
-//                     len=1;
-//                     i=j;
-//                     j++;
-//                 }
+//                 // else
+//                 hash[s[j]]=1;
+//                 int len=j-i+1;
+//                 maxlen=max(maxlen,len);
+
 //             }
 //         }
 //         return maxlen;
 //     }
 // };
 
-// 1.Brute
+// 2.Optimal (Sliding window)
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n=s.size();
+        int l=0,r=0;
         int maxlen=0;
-        for(int i=0;i<n;i++)
+        int hashlen=256;
+        int hash[hashlen];
+        // Initialize hashlen to -1 indicating no last occurrences at start.
+        for(int i=0;i<hashlen;i++)
         {
-            vector<int> hash(256,0);
-            for(int j=i;j<n;j++)
+            hash[i]=-1;
+        }
+        while(r<n)
+        {
+            if(hash[s[r]]!=-1)
             {
-                if(hash[s[j]]==1) {
-                    break;// since character already present thus duplicate
-                }
-                // else
-                hash[s[j]]=1;
-                int len=j-i+1;
-                maxlen=max(maxlen,len);
-
+                l=max(hash[s[r]]+1,l);
             }
+            int len = r-l+1;
+            maxlen=max(maxlen,len);
+
+            hash[s[r]]=r;
+            r++;
         }
         return maxlen;
     }
 };
-
-// 2.Optimal
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-        
-//     }
-// };
