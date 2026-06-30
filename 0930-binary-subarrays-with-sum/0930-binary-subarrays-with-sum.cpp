a3fb78724e00ac3827cb0d78eae85a6939fb0d51
Yash Raj Ravi <yrr6735@gmail.com>
2026-06-30 09:39:53 +0530

    Time: 0 ms (100%), Space: 32.7 MB (62.94%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -17,46 +17,56 @@
 //     }
 // };
 
-// 2.Better
-class Solution {
-public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int,int> prefixSum;
-        prefixSum[0]=1;
-        int n = nums.size();
-        int sum=0;
-        int count=0;
-        for(int i=0;i<n;i++)
-        {
-            sum+= nums[i];
-            if(prefixSum.find(sum-goal)!=prefixSum.end())
-            {
-                count+= prefixSum[sum-goal];
-            }
-            prefixSum[sum]++;
-        }
-        return count;
-    }
-};
-
-// // 3.Optimal
+// // 2.Better
 // class Solution {
 // public:
 //     int numSubarraysWithSum(vector<int>& nums, int goal) {
-//         int l=0; int n = nums.size();
-//         int count=0;int sum=0;
-//         for(int r=0;r<n;r++)
+//         unordered_map<int,int> prefixSum;
+//         prefixSum[0]=1;
+//         int n = nums.size();
+//         int sum=0;
+//         int count=0;
+//         for(int i=0;i<n;i++)
 //         {
-//             sum+= nums[r];
-//             if(sum==goal)
+//             sum+= nums[i];
+//             if(prefixSum.find(sum-goal)!=prefixSum.end())
 //             {
-//                 count++;
+//                 count+= prefixSum[sum-goal];
 //             }
-//             else if(sum>goal)
-//             {
-//                 l++;
-//             }
+//             prefixSum[sum]++;
 //         }
 //         return count;
 //     }
 // };
+
+// // 3.Optimal
+class Solution {
+public:
+
+int atmost(int x,vector<int>& nums)
+{
+
+     int l=0; int n = nums.size();
+     int count=0;int total=0;
+     int sum=0;
+      for(int r=0;r<n;r++)
+        {
+           sum+= nums[r];
+            while(sum>x)
+            {
+                sum-= nums[l];
+                l++;
+            }
+            count=r-l+1;
+            total+= count;
+        }
+        return total;
+}
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        if(goal==0)
+        {
+            return atmost(goal,nums);
+        }
+       return atmost(goal,nums)-atmost(goal-1,nums);
+    }
+};
