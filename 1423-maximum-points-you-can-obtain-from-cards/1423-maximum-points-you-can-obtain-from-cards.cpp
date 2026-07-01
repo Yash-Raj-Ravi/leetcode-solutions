03a2a4dc637ab0ba94b1d68e3b59cd47a356ede6
Yash Raj Ravi <yrr6735@gmail.com>
2026-07-01 09:46:13 +0530

    Time: 0 ms (100%), Space: 46.3 MB (63.83%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,4 +1,29 @@
-// 1.Brute
+// // 1.Brute
+// class Solution {
+// public:
+//     int maxScore(vector<int>& cardPoints, int k) {
+//         int n= cardPoints.size();
+//         int maxScore=0;
+//         for(int i=0;i<=k;i++) // Loop will run till k included since for k, we have k+1 ways of taking cards.
+//         {
+//             int score=0;
+//             // Cards taken from front
+//             for(int j=0;j<i;j++)
+//             {
+//                 score+= cardPoints[j];
+//             }
+//             // Cards taken from back
+//             for(int j=0;j<k-i;j++)
+//             {
+//                 score+= cardPoints[n-1-j];
+//             }
+//             maxScore=max(maxScore,score);// maxScore updated after cards have been taken from front and back combining to k number of cards.
+//         }
+//         return maxScore;
+//     }
+// };
+
+// 2.Optimal
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
@@ -24,11 +49,3 @@ public:
      return maxScore;
     }
 };
-
-// 2.Optimal
-// class Solution {
-// public:
-//     int maxScore(vector<int>& cardPoints, int k) {
-        
-//     }
-// };
