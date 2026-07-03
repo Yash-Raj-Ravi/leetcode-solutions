280dbe3e79cdfa8416e47f1c86748e18c17277ac
Yash Raj Ravi <yrr6735@gmail.com>
2026-07-03 22:38:32 +0530

    Time: 80 ms (8.86%), Space: 77.9 MB (6.41%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -5,7 +5,6 @@ public:
         vector<int> ans;
         unordered_map<int,int> freq;
         int n = nums.size();
-        sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++)
         {
             freq[nums[i]]++;
