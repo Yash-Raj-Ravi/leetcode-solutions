7406f4337f4c67fce291dc51424bfab99494c4eb
Yash Raj Ravi <yrr6735@gmail.com>
2026-07-06 12:02:11 +0530

    Time: 0 ms (100%), Space: 87.3 MB (11.37%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,26 +1,62 @@
+// class Solution {
+// public:
+//     bool lemonadeChange(vector<int>& bills) {
+//         int n = bills.size();
+//         unordered_map<int,int> changefreq;
+//         for(int i=0;i<n;i++)
+//         {
+//             changefreq[bills[i]]++;
+//             if(bills[i]==10)
+//             {
+//                 if(changefreq[5]==0) return false;
+//                 else
+//                  changefreq[5]--;
+//             }
+//             else if(bills[i]==20)
+//             {
+//               if(changefreq[5]>0 && changefreq[10]>0)
+//                 {
+//                     changefreq[5]--;changefreq[10]--;
+//                 }
+//                 else if(changefreq[5]>=3)
+//                 {
+//                     changefreq[5]-=3;
+//                 }
+//                 else
+//                  return false;
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+// Using Counter instead of map
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
         int n = bills.size();
-        unordered_map<int,int> changefreq;
+        int countfive=0;
+        int countten=0;
         for(int i=0;i<n;i++)
         {
-            changefreq[bills[i]]++;
+            if (bills[i]==5) countfive++;
             if(bills[i]==10)
             {
-                if(changefreq[5]==0) return false;
+                if(countfive>0) 
+                 {countfive--;
+                 countten++;}
                 else
-                 changefreq[5]--;
+                 return false;
             }
             else if(bills[i]==20)
             {
-              if(changefreq[5]>0 && changefreq[10]>0)
+              if(countfive>0 && countten>0)
                 {
-                    changefreq[5]--;changefreq[10]--;
+                    countfive--;countten--;
                 }
-                else if(changefreq[5]>=3)
+                else if(countfive>=3)
                 {
-                    changefreq[5]-=3;
+                    countfive-=3;
                 }
                 else
                  return false;
