8d5b22ed7560ba360e98c0068a9e895272f66fd2
Yash Raj Ravi <yrr6735@gmail.com>
2026-07-07 11:57:11 +0530

    Time: 71 ms (18.1%), Space: 21.9 MB (14.2%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,53 @@
+// // 1.Brute
+// class Solution {
+// public:
+
+// int helper(vector<int>& nums,int pos)
+// {
+//     int n = nums.size();
+//     int minJump=INT_MAX;
+//     if(pos >= n-1) return 0;
+
+//     if(nums[pos]==0) return INT_MAX;
+
+//     for(int jump=1;jump<=nums[pos];jump++)
+//     {
+//         int subresult = helper(nums,pos+jump);
+
+//         if(subresult!=INT_MAX)
+//         {
+//             minJump = min(minJump,1+subresult);
+//         }
+//     }
+// return minJump;
+// }
+//     int jump(vector<int>& nums) {
+//         return helper(nums,0);
+//     }
+// };
+
+// 2.Better (DP-Array)
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,INT_MAX); // Stores min jumps required to reach each index
+        dp[0]=0;
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=nums[i] && i+j<n; j++){
+            dp[i+j] = min(dp[i+j],dp[i]+1);
+            }
+        }
+        return dp[n-1];
+    }
+};
+
+
+// class Solution {
+// public:
+//     int jump(vector<int>& nums) {
+        
+//     }
+// };
