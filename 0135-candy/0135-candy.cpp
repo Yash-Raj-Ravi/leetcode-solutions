9eceee48cce2f488a69f4eb07991c73dc23ac966
Yash Raj Ravi <yrr6735@gmail.com>
2026-07-11 10:50:48 +0530

    Time: 0 ms (100%), Space: 22 MB (97.95%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -14,7 +14,7 @@
 //                 assigned[i] = assigned[i+1]+1;
 //                 changed = true;
 //             }
-//            else if(ratings[i]<ratings[i+1] && assigned[i]>=assigned[i+1])
+//             if(ratings[i]<ratings[i+1] && assigned[i]>=assigned[i+1])
 //             {
 //                 assigned[i+1] = assigned[i]+1;
 //                 changed = true;
@@ -25,37 +25,62 @@
 //     }
 // };
 
-// 2.Better
-class Solution {
-public:
-    int candy(vector<int>& ratings) {
-         int n = ratings.size();
-        vector<int> assigned(n,1); // Tracks the number of assigned candies to a child
- 
-        for(int i=0;i<n-1;i++) // left to right pass
-        {
-            if(ratings[i+1]>ratings[i])
-            {
-                assigned[i+1] = assigned[i]+1;
-            }
-        }
-
-        for(int i=n-1;i>0;i--) // right to left pass
-        {
-            if(ratings[i-1]>ratings[i])
-            {
-                assigned[i-1] = max(assigned[i-1],assigned[i]+1);
-            }
-        }
-    
-        return accumulate(assigned.begin(),assigned.end(),0);
-    }
-};
-
-
+// // 2.Better
 // class Solution {
 // public:
 //     int candy(vector<int>& ratings) {
-        
+//          int n = ratings.size();
+//         vector<int> assigned(n,1); // Tracks the number of assigned candies to a child
+ 
+//         for(int i=0;i<n-1;i++) // left to right pass
+//         {
+//             if(ratings[i+1]>ratings[i])
+//             {
+//                 assigned[i+1] = assigned[i]+1;
+//             }
+//         }
+
+//         for(int i=n-1;i>0;i--) // right to left pass
+//         {
+//             if(ratings[i-1]>ratings[i])
+//             {
+//                 assigned[i-1] = max(assigned[i-1],assigned[i]+1);
+//             }
+//         }
+    
+//         return accumulate(assigned.begin(),assigned.end(),0);
 //     }
 // };
+
+// 3.Optimal
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int peak=0,valley=0;
+        int n = ratings.size();
+        int ans=n;
+
+        for(int i=0;i<n-1;)
+        {
+            peak=0,valley=0;
+            if(ratings[i]==ratings[i+1])
+            {
+                i++; continue;
+            }
+            while(i<n-1 && ratings[i+1] > ratings[i])
+            {
+                peak++;
+                ans+= peak;
+                i++;
+            }
+            while(i<n-1 && ratings[i+1] < ratings[i])
+            {
+                valley++;
+                ans+= valley;
+                i++;
+            }
+            ans-= min(peak,valley);
+        }
+        return ans;
+    }
+};
