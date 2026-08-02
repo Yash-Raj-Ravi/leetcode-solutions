460d17d1668302937679eadbaf92fb3e015329ec
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-02 17:54:19 +0530

    Time: 0 ms (100%), Space: 10.1 MB (90.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -22,28 +22,39 @@
 //     }
 // };
 
-// 2.Tabulation
+// // 2.Tabulation
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n+2,0);
+//         dp[n]=0; dp[n+1]=0;
+//         for(int i=n-1;i>=0;i--)
+//         {
+//            int rob = nums[i] + dp[i+2]; 
+//            int skip = dp[i+1]; 
+//            dp[i] = max(rob,skip);
+//         }
+
+//     return dp[0];
+//     }
+// };
+
+// 3.Space Optimization
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+2,0);
-        dp[n]=0; dp[n+1]=0;
+        int next=0;
+        int furthernext=0;
         for(int i=n-1;i>=0;i--)
         {
-           int rob = nums[i] + dp[i+2]; 
-           int skip = dp[i+1]; 
-           dp[i] = max(rob,skip);
+            int rob = nums[i] + furthernext;
+            int skip = next;
+            int curr = max(skip,rob);
+            furthernext = next;
+            next = curr;
         }
-
-    return dp[0];
+        return next;
     }
 };
-
-
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-        
-//     }
-// };
