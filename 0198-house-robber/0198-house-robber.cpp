8c462c93f8a940020b0366ab6d55559ebaa2c6cc
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-02 17:44:34 +0530

    Time: 0 ms (100%), Space: 10.8 MB (24.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,34 +1,44 @@
-// 1.Memoization
-class Solution {
-private:
-    int solve(int idx,vector<int> &dp,const vector<int> &nums)
-    {
-        int n = nums.size();
-        // Base case: Forward Memoization
-        if(idx>=n) return 0; // Since if houses are existing say till index 4 then nothing can be robbed for index 5 onwards.
+// // 1.Memoization
+// class Solution {
+// private:
+//     int solve(int idx,vector<int> &dp,const vector<int> &nums)
+//     {
+//         int n = nums.size();
+//         // Base case: Forward Memoization
+//         if(idx>=n) return 0; // Since if houses are existing say till index 4 then nothing can be robbed from index 5 onwards.
 
-        if(dp[idx]!=-1) return dp[idx];
+//         if(dp[idx]!=-1) return dp[idx];
 
-        int rob = nums[idx] + solve(idx+2,dp,nums);
-        int skip = solve(idx+1,dp,nums);
+//         int rob = nums[idx] + solve(idx+2,dp,nums);
+//         int skip = solve(idx+1,dp,nums);
         
-    return dp[idx] = max(rob,skip);
-    }
+//     return dp[idx] = max(rob,skip);
+//     }
+// public:
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n,-1);
+//         return solve(0,dp,nums);
+//     }
+// };
+
+// 2.Tabulation
+class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,-1);
-        return solve(0,dp,nums);
+        vector<int> dp(n+2,0);
+        dp[n]=0; dp[n+1]=0;
+        for(int i=n-1;i>=0;i--)
+        {
+           int rob = nums[i] + dp[i+2]; 
+           int skip = dp[i+1]; 
+           dp[i] = max(rob,skip);
+        }
+
+    return dp[0];
     }
 };
-
-
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-        
-//     }
-// };
 
 
 // class Solution {
