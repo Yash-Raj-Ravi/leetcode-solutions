7f0638365ece71f55e416b76da5e1c71dc66b146
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-08 22:34:45 +0530

    Time: 0 ms (100%), Space: 8.2 MB (82.97%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -19,35 +19,54 @@
 //     }
 // };
 
-// 2.Tabulation
+// // 2.Tabulation
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m,vector<int> (n,0));
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(i==0 && j==0) dp[i][j]=1;
+
+//                 else
+//                 {
+//                     int up=0; int left=0;
+//                     if(i>0) up = dp[i-1][j];
+//                     if(j>0) left = dp[i][j-1];
+
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// };
+
+// 3. Space Optimization
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,0));
+        vector<int> prev(n,0);
         for(int i=0;i<m;i++)
         {
+            vector<int> curr(n,0);
             for(int j=0;j<n;j++)
             {
-                if(i==0 && j==0) dp[i][j]=1;
+                if(i==0 && j==0) curr[j]=1;
 
                 else
                 {
                     int up=0; int left=0;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
 
-                    dp[i][j] = up + left;
+                    curr[j] = up + left;
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
-
-
-// class Solution {
-// public:
-//     int uniquePaths(int m, int n) {
-        
-//     }
-// };
