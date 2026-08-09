dcfdae7ef4b2d3625a3b642ad3866ed64ce90043
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-09 11:08:37 +0530

    Time: 4 ms (15.55%), Space: 15.8 MB (78.4%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -22,36 +22,56 @@
 //     }
 // };
 
-// 2.Tabulation
+// // 2.Tabulation
+// class Solution {
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(i==0 && j==0) dp[i][j]=grid[i][j];
+//                 else
+//                 {
+//                     int up=INT_MAX,left=INT_MAX;
+//                     if(i>0) up = dp[i-1][j];
+//                     if(j>0) left = dp[i][j-1];
+//                     dp[i][j] = grid[i][j] + min(up,left);
+//                 }
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// };
+
+// 3.Space Optimization
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<int> prev(n,0);
 
         for(int i=0;i<m;i++)
         {
+            vector<int> curr(n,0);
             for(int j=0;j<n;j++)
             {
-                if(i==0 && j==0) dp[i][j]=grid[i][j];
+                if(i==0 && j==0) curr[j]=grid[i][j];
                 else
                 {
                     int up=INT_MAX,left=INT_MAX;
-                    if(i>0) up = dp[i-1][j];
-                    if(j>0) left = dp[i][j-1];
-                    dp[i][j] = grid[i][j] + min(up,left);
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j] = grid[i][j] + min(up,left);
                 }
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
-
-
-// class Solution {
-// public:
-//     int minPathSum(vector<vector<int>>& grid) {
-        
-//     }
-// };
