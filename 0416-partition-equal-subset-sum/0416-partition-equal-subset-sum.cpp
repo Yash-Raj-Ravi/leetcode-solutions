a0840465b0d68364cb70f1f5c8209fcf5b37cec1
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-11 23:22:59 +0530

    Time: 157 ms (30.83%), Space: 17.4 MB (66.37%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,90 @@
+// // 1.Recursion
+// class Solution {
+// private:
+//     bool solve(int idx,int target,vector<int>& nums)
+//     {
+//         int n = nums.size();
+//         if(target==0) return true;
+//         if(idx==n) return false;
+
+//         // Skip
+//         bool notTake = solve(idx+1,target,nums);
+//         // Take
+//         bool Take = false;
+//         if(target>=nums[idx]) Take = solve(idx+1,target-nums[idx],nums);
+
+//         return Take||notTake;
+//     }
+// public:
+//     bool canPartition(vector<int>& nums) {
+//         int sum = accumulate(nums.begin(),nums.end(),0);
+//         if(sum%2==1) return false;
+//         return solve(0,sum/2,nums);
+//     }
+// };
+
+// // 2.Memoization
+// class Solution {
+// private:
+//     bool solve(int idx,vector<int>& nums,int target,vector<vector<int>> &dp)
+//     {
+//         int n = nums.size();
+//         // Base case:
+//         if(target==0) return true;
+//         if(idx == 0) return target==nums[0];
+
+//         if(dp[idx][target]!=-1) return dp[idx][target];
+//         // skip the value at arr[idx]
+//         bool notTake = solve(idx-1,nums,target,dp);
+//         // Take the value at arr[idx]
+//         bool take = false; 
+//         if(target>=nums[idx])
+//           take = solve(idx-1,nums,target-nums[idx],dp);
+//     return dp[idx][target] = take || notTake; // Since target can be achived in either case.
+
+//     }
+// public:
+//     bool canPartition(vector<int>& nums) {
+//         int n = nums.size();
+//         int sum = accumulate(nums.begin(),nums.end(),0);
+//         if(sum%2==1) return false;
+//         vector<vector<int>> dp(n,vector<int>((sum/2)+1,-1)); 
+//         return solve(n-1,nums,sum/2,dp);
+//     }
+//  };
+
+// 3.Tabulation
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2==1) return false;
+        int target = sum/2;
+        vector<vector<bool>> dp(n,vector<bool>(target+1,0));
+        for(int i=0;i<n;i++) dp[i][0] = true; // If at any index of the array, target becomes zero then return true.
+        if(nums[0] <= target) dp[0][nums[0]] = true;
+
+        for(int idx=1;idx<n;idx++)
+        {
+            for(int j=1;j<=target;j++)
+            {
+                bool notTake = dp[idx-1][j];
+                // Take the value at arr[idx]
+                bool take = false; 
+                if(j>=nums[idx])
+                take = dp[idx-1][j-nums[idx]];
+                dp[idx][j] = (take || notTake); 
+            }
+        }
+    return dp[n-1][target];
+    }
+};
+
+
+// class Solution {
+// public:
+//     bool canPartition(vector<int>& nums) {
+        
+//     }
+// };
