13dcee46cb47f18e30c447a919a91de45537be80
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-22 12:37:27 +0530

    Time: 8 ms (6.84%), Space: 9.5 MB (92.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,11 +3,13 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         int n = s.size();
+
+        if(n!=t.size()) return false;
+
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
 
-        if(s==t) return true;
-        return false;
+        return s==t; 
     }
 };
 
