945698422bd1adaf1210a909fe7d3a8328c2a09a
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-22 10:42:26 +0530

    Time: 1 ms (42.6%), Space: 18.7 MB (12.12%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -5,7 +5,7 @@ public:
         string ans="";
         for(int i=n-1;i>=0;i--)
         {
-            if(num[i]%2==1) 
+            if((num[i]-'0')%2==1) 
             {
                 ans+= num.substr(0,i+1); break;
             }
