6e72cf0592181d008ae112b4cb0b6ccbb0c646ac
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-22 12:38:40 +0530

    Time: 8 ms (6.84%), Space: 9.5 MB (99.38%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -9,9 +9,28 @@ public:
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
 
-        return s==t; 
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]!=t[i]) return false;
+        }
+        return true;
     }
 };
+
+// // 2.Brute (Direct)
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         int n = s.size();
+
+//         if(n!=t.size()) return false;
+
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+
+//         return s==t; 
+//     }
+// };
 
 
 // class Solution {
