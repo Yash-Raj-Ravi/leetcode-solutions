cfd7677c39648482f98bf075b9bd10d55fbd54b4
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-22 10:24:13 +0530

    Time: 0 ms (100%), Space: 10 MB (79.29%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,55 +1,63 @@
-// 1.Brute
-class Solution {
-public:
-    string reverseWords(string s) {
-        vector<string> words; // To store list of words
-        string word = "";     // To collect each valid word
-        int n = s.size();
-
-        for (int i = 0; i < n; i++) {
-            if (s[i] != ' ')
-                word.push_back(s[i]);
-            else if (!word.empty()) // If space encountered
-            {
-                words.push_back(word);
-                word = ""; // Reset the word for next word formation
-            } 
-        }
-        if (!word.empty())
-            words.push_back(word); // For last word
-
-        reverse(words.begin(), words.end());
-        string ans = "";
-        for (int j = 0; j < words.size(); j++) {
-            ans += words[j];
-            // Add a space if it's not the last word
-            if (j < words.size() - 1) {
-                ans += " ";
-            }
-        }
-
-        return ans;
-    }
-};
+// // 1.Brute
 // class Solution {
 // public:
 //     string reverseWords(string s) {
-//         string ans = "";
+//         vector<string> words; // To store list of words
+//         string word = "";     // To collect each valid word
 //         int n = s.size();
-//         int prev = n;
-//         for(int i=n-1;i>=0;i--)
-//         {
-//             if(s[i]==' ')
+
+//         for (int i = 0; i < n; i++) {
+//             if (s[i] != ' ')
+//                 word.push_back(s[i]);
+//             else if (!word.empty()) // If space encountered
 //             {
-//                 for(int j=0;j<prev-i-1;j++)
-//                 {
-//                     if(s[i+j+1]!=' ')
-//                      ans+= s[i+j+1];
-//                 }
-//                 ans+=' ';
-//                 prev = i;
+//                 words.push_back(word);
+//                 word = ""; // Reset the word for next word formation
 //             }
 //         }
-//     return ans;
+//         if (!word.empty())
+//             words.push_back(word); // For last word
+
+//         reverse(words.begin(), words.end());
+//         string ans = "";
+//         for (int j = 0; j < words.size(); j++) {
+//             ans += words[j];
+//             // Add a space if it's not the last word
+//             if (j < words.size() - 1) {
+//                 ans += " ";
+//             }
+//         }
+
+//         return ans;
 //     }
 // };
+
+// 2.Optimal
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans = "";
+        int n = s.size();
+        int i = n - 1;
+        while (i >= 0) {
+
+            while (i >= 0 && s[i] == ' ')
+                i--;
+
+            if (i < 0)
+                break;
+            int end = i;
+
+            while (i >= 0 && s[i] != ' ')
+                i--;
+
+            string word = s.substr(i + 1, end - i);
+
+            if (!ans.empty()) {
+                ans += ' ';
+            }
+            ans += word;
+        }
+        return ans;
+    }
+ };
