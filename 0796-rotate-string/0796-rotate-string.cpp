cce0984c95c0334b3938b9997091cde2c7e4f59a
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-22 11:50:36 +0530

    Time: 0 ms (100%), Space: 8.1 MB (57.06%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,24 +1,28 @@
-// 1.Brute
+// // 1.Brute
+// class Solution {
+// public:
+//     bool rotateString(string s, string goal) {
+//         if (s.size() != goal.size()) return false;
+//         int n = s.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             if(s.compare(goal)==0) return true;
+//             char temp = s[0];
+//             s.erase(0,1);
+//             s+= temp;
+//         }
+//         return false;
+//     }
+// };
+
+// 2.Optimal
+// Goal if present must be found in s+s;
 class Solution {
 public:
     bool rotateString(string s, string goal) {
         if (s.size() != goal.size()) return false;
-        int n = s.size();
-        for(int i=0;i<n;i++)
-        {
-            if(s.compare(goal)==0) return true;
-            char temp = s[0];
-            s.erase(0,1);
-            s+= temp;
-        }
-        return false;
+        string doubled = s+s;
+        return doubled.find(goal) != string::npos;
+        // string::npos means "not found" when used with string searching functions like find().
     }
 };
-
-// 2.Optimal
-// class Solution {
-// public:
-//     bool rotateString(string s, string goal) {
-        
-//     }
-// };
