2039c80c1bd05e0b4615d6393597830bf2739e4d
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-22 12:43:30 +0530

    Time: 0 ms (100%), Space: 9.7 MB (49.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,21 @@
-// 1.Brute
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        int n = s.size();
+// // 1.Brute
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         int n = s.size();
 
-        if(n!=t.size()) return false;
+//         if(n!=t.size()) return false;
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
 
-        for(int i=0;i<n;i++)
-        {
-            if(s[i]!=t[i]) return false;
-        }
-        return true;
-    }
-};
+//         for(int i=0;i<n;i++)
+//         {
+//             if(s[i]!=t[i]) return false;
+//         }
+//         return true;
+//     }
+// };
 
 // // 2.Brute (Direct)
 // class Solution {
@@ -32,10 +32,27 @@ public:
 //     }
 // };
 
+// 3.Optimal
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n = s.size();
 
-// class Solution {
-// public:
-//     bool isAnagram(string s, string t) {
-        
-//     }
-// };
+        if(n!=t.size()) return false;
+        int freq[26] = {0};
+
+        for(auto x:s)
+        {
+            freq[x-'a']++;
+        }
+        for(auto x:t)
+        {
+            freq[x-'a']--;
+        }
+        for(int x:freq)
+        {
+            if(x!=0) return false;
+        }
+        return true;
+    }
+};
