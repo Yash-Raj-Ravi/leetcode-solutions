4d83fd60712c6d00d6e1c1b547e0e79f74b53da3
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-25 21:02:53 +0530

    Time: 0 ms (100%), Space: 10 MB (59.36%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,36 +1,79 @@
+// 1.Using 1 queue
+// class MyStack {
+
+// private:
+//        queue<int> q;
+// public:
+//     MyStack() {
+  
+//     }
+    
+//     void push(int x) {
+//         int sz = q.size();
+//         q.push(x);
+//         for(int i=0; i<sz; i++)
+//         {
+//             q.push(q.front());
+//             q.pop();
+//         } 
+//     }
+    
+//     int pop() {
+//         int top = q.front();
+//         q.pop();
+//         return top;
+//     }
+    
+//     int top() {
+//         return q.front();
+//     }
+    
+//     bool empty() {
+//         return q.size()==0;
+//     }
+// };
+
+// 2.Using 2 queues
 class MyStack {
 
 private:
-       queue<int> q;
+    queue<int> q1, q2;
+
 public:
     MyStack() {
-  
+        
     }
     
     void push(int x) {
-        int sz = q.size();
-        q.push(x);
-        for(int i=0; i<sz; i++)
+        q2.push(x);
+
+        while(!q1.empty())
         {
-            q.push(q.front());
-            q.pop();
-        } 
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        swap(q1, q2);
     }
     
     int pop() {
-        int top = q.front();
-        q.pop();
+        int top = q1.front();
+        q1.pop();
         return top;
     }
     
     int top() {
-        return q.front();
+        return q1.front();
     }
     
     bool empty() {
-        return q.size()==0;
+        return q1.empty();
     }
 };
+
+
+
+
 
 /**
  * Your MyStack object will be instantiated and called as such:
