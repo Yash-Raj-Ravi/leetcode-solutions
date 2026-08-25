f913e203d4598e63fc55bf2c0ddfdf283d2c0138
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-25 17:58:05 +0530

    Time: 7 ms (96.2%), Space: 8.8 MB (95.86%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -87,7 +87,42 @@
 //     }
 // };
 
-// // 4.Space Optimization
+// // // 4.Space Optimization
+// class Solution {
+// public:
+//     int numDistinct(string s, string t) {
+//         int m = s.size();
+//         int n = t.size();
+
+//         if(t.size()>s.size()) return 0;
+
+//         vector<long long> prev(n+1,0);
+//         vector<long long> curr(n+1,0);
+
+//         prev[0] = 1;
+
+//         for(int idx1=1;idx1<=m;idx1++)
+//         {
+//             curr[0]=1;
+//             for(int idx2=1;idx2<=n;idx2++)
+//             {
+//                 if(s[idx1-1]==t[idx2-1]){
+//             // If the chracter matches , Take it and continue as well as also consider the case of continuing without taking it to look for another occurrence of it.
+//                 long long ways = (long long) prev[idx2-1] + prev[idx2];
+//                 curr[idx2] = min(ways,(long long)INT_MAX); // To avoid overflow error
+//             }
+//             else
+//             {
+//               curr[idx2] =  prev[idx2];
+//             }
+//         }
+//         prev = curr;
+//         }
+//     return prev[n];
+//     }
+// };
+
+// // 5.Space Optimization (Using one 1D array)
 class Solution {
 public:
     int numDistinct(string s, string t) {
@@ -97,26 +132,21 @@ public:
         if(t.size()>s.size()) return 0;
 
         vector<long long> prev(n+1,0);
-        vector<long long> curr(n+1,0);
 
         prev[0] = 1;
 
         for(int idx1=1;idx1<=m;idx1++)
         {
-            curr[0]=1;
-            for(int idx2=1;idx2<=n;idx2++)
+            for(int idx2=n;idx2>=1;idx2--)
             {
                 if(s[idx1-1]==t[idx2-1]){
             // If the chracter matches , Take it and continue as well as also consider the case of continuing without taking it to look for another occurrence of it.
                 long long ways = (long long) prev[idx2-1] + prev[idx2];
-                curr[idx2] = min(ways,(long long)INT_MAX); // To avoid overflow error
+                prev[idx2] = min(ways,(long long)INT_MAX); // To avoid overflow error
             }
-            else
-            {
-              curr[idx2] =  prev[idx2];
-            }
+         
         }
-        prev = curr;
+  
         }
     return prev[n];
     }
