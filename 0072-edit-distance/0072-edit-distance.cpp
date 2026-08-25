fed64cf70ab38fdc70deb651f501660b2046f214
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-25 19:55:05 +0530

    Time: 9 ms (38.1%), Space: 13.3 MB (32.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -29,43 +29,68 @@
 //     }
 // };
 
-// 2.Memoization
-class Solution {
-private:
-    int count(int i,int j,string &word1, string &word2,vector<vector<int>> &dp)
-    {
-        // Base cases:
-        // If word1 is traversed fully, Then it needs operations = word2's remaining size to make them both equal
-        if(i<0) return j+1;
-        // If word2 is traversed fully, Then it needs operations = word1's remaining size to make them both equal
-        if(j<0) return i+1;
+// // 2.Memoization
+// class Solution {
+// private:
+//     int count(int i,int j,string &word1, string &word2,vector<vector<int>> &dp)
+//     {
+//         // Base cases:
+//         // If word1 is traversed fully, Then it needs operations = word2's remaining size to make them both equal
+//         if(i<0) return j+1;
+//         // If word2 is traversed fully, Then it needs operations = word1's remaining size to make them both equal
+//         if(j<0) return i+1;
         
-        if(dp[i][j]!=-1) return dp[i][j];
+//         if(dp[i][j]!=-1) return dp[i][j];
 
-        if(word1[i]==word2[j]) return dp[i][j] = count(i-1,j-1,word1,word2,dp); // No operations required
+//         if(word1[i]==word2[j]) return dp[i][j] = count(i-1,j-1,word1,word2,dp); // No operations required
 
-        //else
+//         //else
 
-        // Take the minimum of insert,delete and replace operations
-        int r = 1 + count(i-1,j-1,word1,word2,dp); // Replace
-        int in = 1 + count(i,j-1,word1,word2,dp); // Insert
-        int d = 1 + count(i-1,j,word1,word2,dp); // Delete
+//         // Take the minimum of insert,delete and replace operations
+//         int r = 1 + count(i-1,j-1,word1,word2,dp); // Replace
+//         int in = 1 + count(i,j-1,word1,word2,dp); // Insert
+//         int d = 1 + count(i-1,j,word1,word2,dp); // Delete
 
-        return dp[i][j] = min({r,in,d});
-    }
+//         return dp[i][j] = min({r,in,d});
+//     }
+// public:
+//     int minDistance(string word1, string word2) {
+//         int m = word1.size();
+//         int n = word2.size();
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+//         return count(m-1,n-1,word1,word2,dp);
+//     }
+// };
+
+// 3.Tabulation
+class Solution {
 public:
     int minDistance(string word1, string word2) {
         int m = word1.size();
         int n = word2.size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return count(m-1,n-1,word1,word2,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+
+        for(int i=0;i<=n;i++) dp[0][i] = i;
+        for(int j=0;j<=m;j++) dp[j][0] = j;
+
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(word1[i-1]==word2[j-1]) dp[i][j] = dp[i-1][j-1]; // No operations required
+
+                //else
+                else{
+
+                // Take the minimum of insert,delete and replace operations
+                int r = 1 + dp[i-1][j-1]; // Replace
+                int in = 1 + dp[i][j-1]; // Insert
+                int d = 1 + dp[i-1][j]; // Delete
+
+                dp[i][j] = min({r,in,d});
+                }
+            }
+        }
+    return dp[m][n];
     }
 };
-
-
-// class Solution {
-// public:
-//     int minDistance(string word1, string word2) {
-        
-//     }
-// };
