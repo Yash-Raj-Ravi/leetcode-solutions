0c64e15591ca17cc17c51b06534a73b0e8994257
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-27 19:48:25 +0530

    Time: 8 ms (36.24%), Space: 24.6 MB (36.53%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -29,43 +29,64 @@
 //     }
 // };
 
-// 2.Memoization
-class Solution {
-private:
-    int solve(int idx,int buy,vector<int>& prices,vector<vector<int>> &dp)
-    // buy=1 means you are allowed to buy since you are holding any stock
-    // buy=0 means you are not allowed to buy since you are holding a stock
-    {
-        // Base cases
-        int n = prices.size();
-        if(idx==n) return 0;
+// // 2.Memoization
+// class Solution {
+// private:
+//     int solve(int idx,int buy,vector<int>& prices,vector<vector<int>> &dp)
+//     // buy=1 means you are allowed to buy since you are holding any stock
+//     // buy=0 means you are not allowed to buy since you are holding a stock
+//     {
+//         // Base cases
+//         int n = prices.size();
+//         if(idx==n) return 0;
 
-        if(dp[idx][buy]!=-1) return dp[idx][buy];
-        int maxProfit = 0;
-        if(buy){
-            maxProfit = max(-prices[idx] + solve(idx+1,0,prices,dp),solve(idx+1,1,prices,dp)); // buy becomes 0 in first instance after buying one stock since you can't buy further it is sold and prices[idx] is subtracted in profit since buying has to be subtracted from selling to get profit
-        }
+//         if(dp[idx][buy]!=-1) return dp[idx][buy];
 
-        else // buy==0, Already bought now its time to sell
-        {
-            maxProfit = max(prices[idx] + solve(idx+1,1,prices,dp),solve(idx+1,0,prices,dp)); // 1st instance is of selling which leads to buy=1 since you are allowed to buy after selling while 2nd instance is of continuing to other day without selling the stock in order to look for better profit.
-        }
+//         int maxProfit = 0;
+//         if(buy){
+//             maxProfit = max(-prices[idx] + solve(idx+1,0,prices,dp),solve(idx+1,1,prices,dp)); // buy becomes 0 in first instance after buying one stock since you can't buy further it is sold and prices[idx] is subtracted in profit since buying has to be subtracted from selling to get profit
+//         }
+
+//         else // buy==0, Already bought now its time to sell
+//         {
+//             maxProfit = max(prices[idx] + solve(idx+1,1,prices,dp),solve(idx+1,0,prices,dp)); // 1st instance is of selling which leads to buy=1 since you are allowed to buy after selling while 2nd instance is of continuing to other day without selling the stock in order to look for better profit.
+//         }
     
-        return dp[idx][buy] = maxProfit;
-    }
+//         return dp[idx][buy] = maxProfit;
+//     }
 
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<int>> dp(n,vector<int>(2,-1));
+//         return solve(0,1,prices,dp);
+//     }
+// };
+
+// 3.Tabulation
+class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(0,1,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        
+        for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int buy=1;buy>=0;buy--)
+            {
+                int maxProfit = 0;
+                if(buy){
+                    maxProfit = max(-prices[idx] + dp[idx+1][0],dp[idx+1][1]); // buy becomes 0 in first instance after buying one stock since you can't buy further it is sold and prices[idx] is subtracted in profit since buying has to be subtracted from selling to get profit
+                }
+
+                else // buy==0, Already bought now its time to sell
+                {
+                    maxProfit = max(prices[idx] + dp[idx+1][1],dp[idx+1][0]); // 1st instance is of selling which leads to buy=1 since you are allowed to buy after selling while 2nd instance is of continuing to other day without selling the stock in order to look for better profit.
+                }
+
+                dp[idx][buy] = maxProfit;
+            }
+        }
+    return dp[0][1];
     }
 };
-
-
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-        
-//     }
-// };
