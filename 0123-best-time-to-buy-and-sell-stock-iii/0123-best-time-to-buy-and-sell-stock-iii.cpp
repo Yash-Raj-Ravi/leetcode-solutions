5efa3fdbd429b980253a66b3deaac91a4c88c656
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-28 10:51:33 +0530

    Time: 412 ms (38.79%), Space: 211.5 MB (46.82%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -27,42 +27,65 @@
 //     }
 // };
 
-// 2.Memoization
+// // 2.Memoization
+// class Solution {
+// private:
+//     int solve(int idx,int buy,int transactions,vector<int>& prices,vector<vector<vector<int>>> &dp)
+//     {
+//         int n = prices.size();
+//         // Base cases
+//         if(idx==n) return 0;
+//         if(transactions==2) return 0;
+
+//         if(dp[idx][buy][transactions]!=-1) return dp[idx][buy][transactions];
+
+//         int maxProfit = -1e9;
+
+//         if(buy)
+//         {
+//            maxProfit = max(-prices[idx] + solve(idx+1,0,transactions,prices,dp),solve(idx+1,1,transactions,prices,dp));
+//         }
+//         else // Time to sell
+//         {
+//             maxProfit = max(prices[idx] + solve(idx+1,1,transactions+1,prices,dp),solve(idx+1,0,transactions,prices,dp));
+//         }
+//     return dp[idx][buy][transactions] = maxProfit;
+//     }
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int> (3,-1)));
+//         return solve(0,1,0,prices,dp);
+//     }
+// };
+
+// 3.Tabulation
 class Solution {
-private:
-    int solve(int idx,int buy,int transactions,vector<int>& prices,vector<vector<vector<int>>> &dp)
-    {
-        int n = prices.size();
-        // Base cases
-        if(idx==n) return 0;
-        if(transactions==2) return 0;
-
-        if(dp[idx][buy][transactions]!=-1) return dp[idx][buy][transactions];
-
-        int maxProfit = -1e9;
-
-        if(buy)
-        {
-           maxProfit = max(-prices[idx] + solve(idx+1,0,transactions,prices,dp),solve(idx+1,1,transactions,prices,dp));
-        }
-        else // Time to sell
-        {
-            maxProfit = max(prices[idx] + solve(idx+1,1,transactions+1,prices,dp),solve(idx+1,0,transactions,prices,dp));
-        }
-    return dp[idx][buy][transactions] = maxProfit;
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int> (3,-1)));
-        return solve(0,1,0,prices,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int> (3,0)));
+
+     for(int idx=n-1;idx>=0;idx--)
+        {
+            for(int buy=1;buy>=0;buy--)
+            {
+                for(int transactions=0;transactions<2;transactions++)
+                {
+                int maxProfit = -1e9;
+
+                if(buy)
+                {
+                    maxProfit = max(-prices[idx] + dp[idx+1][0][transactions],dp[idx+1][1][transactions]);
+                }
+                else // Time to sell
+                {
+                    maxProfit = max(prices[idx] + dp[idx+1][1][transactions+1],dp[idx+1][0][transactions]);
+                }
+            dp[idx][buy][transactions] = maxProfit;
+                }
+            }
+        }
+        return dp[0][1][0];
     }
 };
-
-
-// class Solution {
-// public:
-//     int maxProfit(vector<int>& prices) {
-        
-//     }
-// };
