db2e6cc299197b7e75200deedb2e96f08cd65405
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-28 10:44:03 +0530

    Time: 464 ms (17.1%), Space: 223.8 MB (30.49%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,68 @@
+// // 1.Recursion
+// class Solution {
+// private:
+//     int solve(int idx,int buy,int transactions,vector<int>& prices)
+//     {
+//         int n = prices.size();
+//         // Base cases
+//         if(idx==n) return 0;
+//         if(transactions==2) return 0;
+
+//         int maxProfit = -1e9;
+
+//         if(buy)
+//         {
+//            maxProfit = max(-prices[idx] + solve(idx+1,0,transactions,prices),solve(idx+1,1,transactions,prices));
+//         }
+//         else // Time to sell
+//         {
+//             maxProfit = max(prices[idx] + solve(idx+1,1,transactions+1,prices),solve(idx+1,0,transactions,prices));
+//         }
+//     return maxProfit;
+//     }
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         return solve(0,1,0,prices);
+//     }
+// };
+
+// 2.Memoization
+class Solution {
+private:
+    int solve(int idx,int buy,int transactions,vector<int>& prices,vector<vector<vector<int>>> &dp)
+    {
+        int n = prices.size();
+        // Base cases
+        if(idx==n) return 0;
+        if(transactions==2) return 0;
+
+        if(dp[idx][buy][transactions]!=-1) return dp[idx][buy][transactions];
+
+        int maxProfit = -1e9;
+
+        if(buy)
+        {
+           maxProfit = max(-prices[idx] + solve(idx+1,0,transactions,prices,dp),solve(idx+1,1,transactions,prices,dp));
+        }
+        else // Time to sell
+        {
+            maxProfit = max(prices[idx] + solve(idx+1,1,transactions+1,prices,dp),solve(idx+1,0,transactions,prices,dp));
+        }
+    return dp[idx][buy][transactions] = maxProfit;
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int> (3,-1)));
+        return solve(0,1,0,prices,dp);
+    }
+};
+
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+        
+//     }
+// };
