e1f7df05aa74f02b0a83ac896adb39ae6a329b1d
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-28 21:41:46 +0530

    Time: 0 ms (100%), Space: 11.3 MB (5.14%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,18 +1,40 @@
-// 1.Recursion
+// // 1.Recursion
+// class Solution {
+// private:
+//     bool solve(int i,int j,string &s, string &t)
+//     {
+//         if(i<0) return true;
+//         if(j<0) return false;
+
+//         if(s[i]==t[j]) return solve(i-1,j-1,s,t);
+//         return solve(i,j-1,s,t);
+//     }
+// public:
+//     bool isSubsequence(string s, string t) {
+//         int m = s.size();
+//         int n = t.size();
+//         return solve(m-1,n-1,s,t);
+//     }
+// };
+
+// // 2.Memoization
 class Solution {
 private:
-    bool solve(int i,int j,string &s, string &t)
+    bool solve(int i,int j,string &s, string &t,vector<vector<int>> &dp)
     {
         if(i<0) return true;
         if(j<0) return false;
 
-        if(s[i]==t[j]) return solve(i-1,j-1,s,t);
-        return solve(i,j-1,s,t);
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        if(s[i]==t[j]) return dp[i][j] = solve(i-1,j-1,s,t,dp);
+        return dp[i][j] = solve(i,j-1,s,t,dp);
     }
 public:
     bool isSubsequence(string s, string t) {
         int m = s.size();
         int n = t.size();
-        return solve(m-1,n-1,s,t);
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        return solve(m-1,n-1,s,t,dp);
     }
 };
