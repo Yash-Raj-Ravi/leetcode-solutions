b1e7b0bca0704feaed1a138a5666ab0745440d47
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-31 23:00:04 +0530

    Time: 5 ms (45.57%), Space: 28.6 MB (39.41%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -29,22 +29,52 @@
 // 2.Space Optimization
 // Rather than actually taking an array of 2*n size, just think of it virtually by using index % n to access the elements circularly.
 
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         int n = nums.size();
+
+//         vector<int> ans(n,-1);
+        
+//         for(int i=0; i<n; i++)
+//         {
+//             for(int j=i+1;j<i+n; j++)
+//             {
+//                 int idx = j%n;
+//                 if(nums[idx] > nums[i]){
+//                     ans[i] = nums[idx]; break;
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+
+// };
+
+// It gets space optimized above but now taking time O(n*n) so now lets optimize both.
+
+// 3.Time + Space optimized
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
+ 
+        stack<int> st;
 
-        vector<int> ans(n,-1);
+        vector<int> ans(n);
         
-        for(int i=0; i<n; i++)
+        for(int i=2*n-1; i>=0; i--)
         {
-            for(int j=i+1;j<i+n; j++)
-            {
-                int idx = j%n;
-                if(nums[idx] > nums[i]){
-                    ans[i] = nums[idx]; break;
-                }
+            while(!st.empty() && st.top()<=nums[i%n]){
+                st.pop();
             }
+            if(i<n){
+            if(st.empty()) ans[i] = -1;
+            else
+             ans[i] = st.top();
+            }
+            st.push(nums[i%n]);
         }
 
         return ans;
