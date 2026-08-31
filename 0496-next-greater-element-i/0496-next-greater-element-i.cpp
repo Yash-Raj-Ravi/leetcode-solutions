a61d5b29886e8756ba55756e51bc872ad4cbb535
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-31 22:00:48 +0530

    Time: 0 ms (100%), Space: 12.8 MB (39.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,11 +1,48 @@
+// 1.Brute
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         int m = nums1.size();
+//         int n = nums2.size();
+//         stack<int> st;
+//         vector<int> nge2(n);
+//         vector<int> nge1(m);
+
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             while(!st.empty() && nums2[i]>=st.top())
+//             {
+//                 st.pop();
+//             }
+//             if(st.empty()) nge2[i] = -1;
+//             else
+//              nge2[i] = st.top();
+//             st.push(nums2[i]);
+//         }
+
+//         for(int i=0;i<m;i++)
+//         {
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(nums1[i]==nums2[j])
+//                 {
+//                     nge1[i] = nge2[j];
+//                 }
+//             }
+//         }
+//         return nge1;
+//     }
+// };
+
+// 2.Optimal
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
         stack<int> st;
-        vector<int> nge2(n);
         vector<int> nge1(m);
+        unordered_map<int,int> mp;
 
         for(int i=n-1;i>=0;i--)
         {
@@ -13,22 +50,17 @@ public:
             {
                 st.pop();
             }
-            if(st.empty()) nge2[i] = -1;
+            if(st.empty()) mp[nums2[i]] = -1;
             else
-             nge2[i] = st.top();
+             mp[nums2[i]]  = st.top();
             st.push(nums2[i]);
         }
 
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                    nge1[i] = nge2[j];
-                }
-            }
+            nge1[i] = mp[nums1[i]];
         }
+
         return nge1;
     }
 };
