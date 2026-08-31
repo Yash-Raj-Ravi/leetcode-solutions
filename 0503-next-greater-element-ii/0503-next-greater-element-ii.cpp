bb85289776d775f61a60d3a0edf440fecd0ca8eb
Yash Raj Ravi <yrr6735@gmail.com>
2026-08-31 22:50:56 +0530

    Time: 101 ms (6.47%), Space: 27.9 MB (97.75%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,26 +1,53 @@
+// 1.My approach (Need Space optimization by avoiding newNums array)
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> newNums(nums); // Initialize new vector with copy of first then append the first array at back.
+//         newNums.insert(newNums.end(),nums.begin(),nums.end()); // "Insert a copy of everything in nums at the end of newNums."
+//         stack<int> st;
+
+//         vector<int> ans(2*n);
+        
+//         for(int i=2*n-1; i>=0; i--)
+//         {
+//             while(!st.empty() && st.top()<=newNums[i]){
+//                 st.pop();
+//             }
+//             if(st.empty()) ans[i] = -1;
+//             else
+//              ans[i] = st.top();
+//             st.push(newNums[i]);
+//         }
+//         vector<int> Finalans(ans.begin(),ans.begin() + n);
+
+//         return Finalans;
+//     }
+
+// };
+
+// 2.Space Optimization
+// Rather than actually taking an array of 2*n size, just think of it virtually by using index % n to access the elements circularly.
+
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();
-        vector<int> newNums(nums);
-        newNums.insert(newNums.end(),nums.begin(),nums.end()); // "Insert a copy of everything in nums at the end of newNums."
-        stack<int> st;
 
-        vector<int> ans(2*n);
+        vector<int> ans(n,-1);
         
-        for(int i=2*n-1; i>=0; i--)
+        for(int i=0; i<n; i++)
         {
-            while(!st.empty() && st.top()<=newNums[i]){
-                st.pop();
+            for(int j=i+1;j<i+n; j++)
+            {
+                int idx = j%n;
+                if(nums[idx] > nums[i]){
+                    ans[i] = nums[idx]; break;
+                }
             }
-            if(st.empty()) ans[i] = -1;
-            else
-             ans[i] = st.top();
-            st.push(newNums[i]);
         }
-        vector<int> Finalans(ans.begin(),ans.begin() + n);
 
-        return Finalans;
+        return ans;
     }
 
 };
