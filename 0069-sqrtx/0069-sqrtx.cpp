c5d5d24cd88a4dec522a959daf026c67092a793b
Yash Raj Ravi <yrr6735@gmail.com>
2026-09-07 23:15:06 +0530

    Time: 0 ms (100%), Space: 8.5 MB (50.93%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,12 +1,37 @@
+// // 1.Brute
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         int val = -1;
+//         if(x==0) return 0;
+//         if(x==1) return 1;
+//         for(int i=1;i<=x;i++){
+//             if(1LL*i*i > x) {val = i-1;break;}
+//         }
+//         return val;
+//     }
+// };
+
+// 2.Using BS
 class Solution {
 public:
     int mySqrt(int x) {
-        int val = -1;
-        if(x==0) return 0;
-        if(x==1) return 1;
-        for(int i=1;i<=x;i++){
-            if(1LL*i*i > x) {val = i-1;break;}
+        int low = 0;
+        int high = x;
+        int ans = 0;
+
+        while(low <= high) {
+            long long mid = low + (high - low) / 2;
+
+            if(mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
         }
-        return val;
+
+        return ans;
     }
 };
