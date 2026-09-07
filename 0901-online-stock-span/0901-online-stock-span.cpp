7de66d7a6720b66f8eecf398c2e12e473a62e68a
Yash Raj Ravi <yrr6735@gmail.com>
2026-09-07 19:29:16 +0530

    Time: 29 ms (67.99%), Space: 97.2 MB (13.77%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,19 +1,45 @@
+// class StockSpanner {
+// public:
+//     // StockSpanner is a constructor here which signifies a fresh start and it will contain an empty array to begin with.
+//     vector<int> arr;
+
+//     StockSpanner() {
+//     }
+    
+//     int next(int price) {
+//         arr.push_back(price);
+//         int cnt = 1;
+//         for(int i = arr.size()-2;i>=0;i--){
+//             if(arr[i]<=price) cnt++;
+//             else break;
+//         }
+//         return cnt;
+//     }
+// };
+
+// We find pge index for each price then push (current price index) - (pge index) to ans array.
 class StockSpanner {
 public:
-    // StockSpanner is a constructor here which signifies a fresh start and it will contain an empty array to begin with.
-    vector<int> arr;
+
+    int idx = -1;
+    stack<pair<int,int>> st;
 
     StockSpanner() {
+        idx = -1; 
     }
     
+    
     int next(int price) {
-        arr.push_back(price);
-        int cnt = 1;
-        for(int i = arr.size()-2;i>=0;i--){
-            if(arr[i]<=price) cnt++;
-            else break;
+        idx = idx + 1;
+        int ans = -1;
+
+        while(!st.empty() && st.top().first <= price){
+            st.pop();
         }
-        return cnt;
+        ans = idx - (st.empty()?-1:st.top().second);
+        st.push({price,idx});
+
+    return ans;
     }
 };
 
