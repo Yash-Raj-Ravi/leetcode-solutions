e2b54e464edf64caccf896e33391dc79f0015df9
Yash Raj Ravi <yrr6735@gmail.com>
2026-09-17 22:12:03 +0530

    Time: 19 ms (6.55%), Space: 15 MB (5.75%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,23 +8,33 @@
  */
 
 // 1.Brute
-// class Solution {
-// public:
-//     bool hasCycle(ListNode *head) {
-        
-//     }
-// };
-
-// 2.Optimal (Slow,Fast)
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow = head, *fast = head;
-        while(fast!=nullptr && fast->next!=nullptr){
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow==fast) return true;
-       }
-       return false;
+        ListNode *temp = head;
+        unordered_map<ListNode*,int> mp;
+
+        while(temp!=nullptr){
+            if(mp.find(temp)!=mp.end()) return true;
+
+            mp[temp] = 1;
+            temp = temp->next;
+
+        }
+        return false;
     }
 };
+
+// // 2.Optimal (Slow,Fast)
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode* slow = head, *fast = head;
+//         while(fast!=nullptr && fast->next!=nullptr){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//             if(slow==fast) return true;
+//        }
+//        return false;
+//     }
+// };
