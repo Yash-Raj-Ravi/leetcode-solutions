b59494804ed00c7ad08a80ce453978c02696ee03
Yash Raj Ravi <yrr6735@gmail.com>
2026-09-18 19:27:13 +0530

    Time: 10 ms (43.45%), Space: 11.2 MB (85.37%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -7,32 +7,39 @@
  * };
  */
 
-// 1.Brute
-class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
-        ListNode *temp = head;
-        unordered_map<ListNode*,int> mp;
-
-        while(temp!=nullptr){
-            if(mp.find(temp)!=mp.end()) return temp;
-            mp[temp] = 1;
-            temp = temp->next;
-        }
-        return nullptr;
-    }
-};
-
-// // 2.Optimal
+// // 1.Brute
 // class Solution {
 // public:
 //     ListNode *detectCycle(ListNode *head) {
-//         ListNode *slow = head, *fast = head;
-//         while(fast!=nullptr && fast->next!=nullptr){
-//             slow = slow->next;
-//             fast = fast->next->next;
-//             if(slow==fast) return slow;
+//         ListNode *temp = head;
+//         unordered_map<ListNode*,int> mp;
+
+//         while(temp!=nullptr){
+//             if(mp.find(temp)!=mp.end()) return temp;
+//             mp[temp] = 1;
+//             temp = temp->next;
 //         }
 //         return nullptr;
 //     }
 // };
+
+// // 2.Optimal
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast) {
+                slow = head;
+                while(slow!=fast){
+                slow=slow->next;
+                fast=fast->next;
+                }
+                if(slow==fast) return fast;
+            };
+        }
+        return nullptr;
+    }
+};
