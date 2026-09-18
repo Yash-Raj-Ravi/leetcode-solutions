fde590f1c152873eb6481efde48ae69287066e02
Yash Raj Ravi <yrr6735@gmail.com>
2026-09-18 22:15:52 +0530

    Time: 0 ms (100%), Space: 117.8 MB (93.76%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -9,36 +9,59 @@
  * };
  */
 
-// 1.Brute
+// // 1.Brute
+// class Solution {
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         if(head == nullptr || head->next == nullptr) return true;
+
+//         ListNode *back = head;
+//         stack<int> st;
+//         while(back!=nullptr){
+//             st.push(back->val);
+//             back = back->next;
+//         }
+
+//         int cnt = 0;
+//         int sz = st.size()/2;
+
+//         while(cnt<sz  && head->val == st.top()){
+//             st.pop();
+//             head = head->next;
+//             cnt++;
+//             }
+//             if(cnt==sz) return true;
+//             return false;
+//     }
+// };
+
+// 2.Optimal
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         if(head == nullptr || head->next == nullptr) return true;
 
-        ListNode *back = head;
-        stack<int> st;
-        while(back!=nullptr){
-            st.push(back->val);
-            back = back->next;
+        ListNode *slow = head, *fast = head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        int cnt = 0;
-        int sz = st.size()/2;
+        ListNode *p = slow, *q=nullptr, *r=nullptr;
+        while(p!=nullptr){
+            r=q;
+            q=p;
+            p=p->next;
+            q->next = r;
 
-        while(cnt<sz  && head->val == st.top()){
-            st.pop();
+        }
+
+        ListNode *move = q;
+        while(move!=nullptr){
+            if(head->val != move->val) return false;
             head = head->next;
-            cnt++;
-            }
-            if(cnt==sz) return true;
-            return false;
+            move = move->next;
+        }
+        return true;
     }
 };
-
-
-// class Solution {
-// public:
-//     bool isPalindrome(ListNode* head) {
-        
-//     }
-// };
