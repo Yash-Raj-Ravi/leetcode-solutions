826fa7fd00e42eb9abe1ede9319439f0863a8383
Yash Raj Ravi <yrr6735@gmail.com>
2026-09-19 19:38:05 +0530

    Time: 0 ms (100%), Space: 19.5 MB (27.19%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -9,38 +9,93 @@
  * };
  */
 
+// // 1.Brute
+// class Solution {
+//     // array to Linked list conversion
+// private:
+//     ListNode *arrToLL(vector<int>& arr){
+//         if(arr.empty()) return nullptr;
 
+//         ListNode *head = new ListNode(arr[0]);
+//         ListNode *temp = head;
+
+//         for(int i=1;i<arr.size();i++){
+//             temp->next = new ListNode(arr[i]);
+//             temp = temp->next;
+//         }
+//         return head;
+//     }
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         vector<int> arr;
+
+//         ListNode *temp1 = list1;
+//         ListNode *temp2 = list2;
+
+//         while(temp1!=nullptr){
+//             arr.push_back(temp1->val);
+//             temp1 = temp1->next;
+//         }
+//         while(temp2!=nullptr){
+//             arr.push_back(temp2->val);
+//             temp2 = temp2->next;
+//         }
+//     sort(arr.begin(),arr.end());
+//         return arrToLL(arr);
+//     }
+// };
+
+// 2.Optimal
 class Solution {
-    // array to Linked list conversion
-private:
-    ListNode *arrToLL(vector<int>& arr){
-        if(arr.empty()) return nullptr;
-
-        ListNode *head = new ListNode(arr[0]);
-        ListNode *temp = head;
-
-        for(int i=1;i<arr.size();i++){
-            temp->next = new ListNode(arr[i]);
-            temp = temp->next;
-        }
-        return head;
-    }
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        vector<int> arr;
+        if(list1 == nullptr && list2 == nullptr) return nullptr;
+        if(list1 == nullptr) return list2;
+        if(list2 == nullptr) return list1;
 
         ListNode *temp1 = list1;
         ListNode *temp2 = list2;
+        ListNode* head = nullptr;
+        ListNode* prev = nullptr;
 
-        while(temp1!=nullptr){
-            arr.push_back(temp1->val);
+        if(temp1->val <= temp2->val) { 
+            head = temp1;
+            prev = head;
             temp1 = temp1->next;
-        }
-        while(temp2!=nullptr){
-            arr.push_back(temp2->val);
+            } 
+        else {
+            head = temp2;
+            prev = head;
             temp2 = temp2->next;
+            }
+
+        while(temp1!=nullptr && temp2!=nullptr){
+            if(temp1->val <= temp2->val){
+               prev->next = temp1;
+               prev = temp1;
+               temp1 = temp1->next;
+            }
+            else{
+               prev->next = temp2;
+               prev = temp2;
+               temp2 = temp2->next;
+            }
         }
-    sort(arr.begin(),arr.end());
-        return arrToLL(arr);
+
+        if(temp1!=nullptr){
+            while(temp1!=nullptr){
+               prev->next = temp1;
+               prev = temp1;
+               temp1 = temp1->next;
+            }
+        }
+        else{
+               while(temp2!=nullptr){
+               prev->next = temp2;
+               prev = temp2;
+               temp2 = temp2->next;
+            }
+        }
+        return head;
     }
 };
